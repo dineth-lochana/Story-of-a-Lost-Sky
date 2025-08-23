18e0e34c35a15e059834db0d72a0a859c441a35b
Dineth Lochana Mallawarachchi <lochandineth@gmail.com>
2025-08-23 23:42:50 +0530

    Test

--- setup-win.py
+++ setup-win.py
@@ -5,6 +5,6 @@ setup(
     windows = [{"script":"srpg.py","icon_resources": [(0, "icon.ico")]}],
     data_files = [ (".", ["README.txt", "bsd_license.txt"]) ],
     options = {'py2exe':{'excludes':["wx",'email',"Tkconstants","Tkinter","tcl","numpy"],
-        'bundle_files':1} # Unbreaks the music
+        'bundle_files':3} # Unbreaks the music
         }
     )
